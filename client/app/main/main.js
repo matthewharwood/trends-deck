9bfc043114be492ea01e869f4d0150e2d04d37aa
Matthew Harwood <matthhar12@gmail.com>
2015-04-24 15:04:28 -0700

    init comit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('trendsDeckApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
